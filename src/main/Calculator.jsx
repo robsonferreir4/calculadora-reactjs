953ab4b38daaa7017a58406e59da179f759d35e7
Paulo Robson da Silva Ferreira <robson.ferreira@sap.ce.gov.br>
2021-02-17 21:44:05 -0300

    tela inicial somente com layout

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+
+    render () {
+        return (
+            <div className="calculator">
+
+                
+            </div>
+
+        )
+    }
+}
+
